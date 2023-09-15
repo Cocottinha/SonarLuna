7316961ea3a2e5c0f73f8f58e877dbed0dfc5c0a
Lucas Cotta Muniz <lucascottamuniz@hotmail.com>
2023-09-15 18:08:27 -0300

    Adicionar arquivos de projeto.

---
+++ SonarLuna/frmMain.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SonarLuna
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(panel1);
+            splitContainer1.Size = new Size(984, 561);
+            splitContainer1.SplitterDistance = 182;
+            splitContainer1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(792, 555);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(792, 555);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(984, 561);
+            Controls.Add(splitContainer1);
+            MaximumSize = new Size(1000, 600);
+            MinimumSize = new Size(1000, 600);
+            Name = "frmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Sonar do Luna";
+            Load += frmMain_Load;
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ SonarLuna/frmMain.cs
@@ -0,0 +1,97 @@
+using System.Drawing.Drawing2D;
+using System.Threading.Tasks;
+using System.Timers;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace SonarLuna
+{
+    public partial class frmMain : Form
+    {
+        public System.Windows.Forms.Timer temporizador = new System.Windows.Forms.Timer();
+
+        int largura = 300, altura = 300, guia = 150;
+
+        int graus;
+        int centerX, centerY;
+        int x, y;
+
+        int tx, ty, lim = 20;
+
+        Bitmap bmp;
+        Pen p;
+        Graphics g;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(largura + 1, altura + 1);
+
+            this.BackColor = Color.Black;
+
+            centerX = largura / 2;
+            centerY = altura / 2;
+
+            graus = 0;
+
+            temporizador.Interval = 30; //ms
+            temporizador.Tick += new EventHandler(timer_Elapsed);
+            temporizador.Start();
+        }
+        private void timer_Elapsed(object sender, EventArgs e)
+        {
+            p = new Pen(Color.LimeGreen, 1f);
+
+            g = Graphics.FromImage(bmp);
+
+            int vrau = (graus - lim) % 360;
+
+            if (graus >= 0 && graus <= 180)
+            {
+                x = centerX + (int)(guia * Math.Sin(Math.PI * graus / 180));
+                y = centerY - (int)(guia * Math.Cos(Math.PI * graus / 180));
+            }
+            else
+            {
+                x = centerX - (int)(guia * -Math.Sin(Math.PI * graus / 180));
+                y = centerY - (int)(guia * Math.Cos(Math.PI * graus / 180));
+            }
+            if (vrau >= 0 && vrau <= 180)
+            {
+                tx = centerX + (int)(guia * Math.Sin(Math.PI * vrau / 180));
+                ty = centerY - (int)(guia * Math.Cos(Math.PI * vrau / 180));
+            }
+            else
+            {
+                tx = centerX - (int)(guia * -Math.Sin(Math.PI * vrau / 180));
+                ty = centerY - (int)(guia * Math.Cos(Math.PI * vrau / 180));
+            }
+
+            g.DrawEllipse(p, 0, 0, largura, altura);
+            g.DrawEllipse(p, 80, 80, largura - 160, altura - 160);
+
+            //linha verde
+            g.DrawLine(p, new Point(centerX, 0), new Point(centerX, altura));
+            g.DrawLine(p, new Point(0, centerY), new Point(largura, centerY));
+
+            //guia
+            g.DrawLine(new Pen(Color.Black, 2f), new Point(centerX, centerY), new Point(tx, ty));
+            g.DrawLine(p, new Point(centerX, centerY), new Point(x, y));
+
+            //carregar a picturebox
+            pictureBox1.Image = bmp;
+            g.Dispose();
+            p.Dispose();
+
+            //atualiza radar
+            graus++;
+            if (graus == 360)
+            {
+                graus = 0;
+            }
+        }
+    }
+}
